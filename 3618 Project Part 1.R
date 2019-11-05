99b926f74af308f1fb69d89a368d1ea4db60a593
Rohan1208 <57326457+Rohan1208@users.noreply.github.com>
2019-11-04 19:04:08 -0500

    Add files via upload

---
+++ 3618 Project Part 1.R
@@ -0,0 +1,6 @@
+rownames = c("Vampire", "Devil")
+colnames = c("KitKat", "Nerds","Reese's", "Smarties", "Twix","Hersey's")
+dimnames = list(rownames, colnames)
+animal <- matrix(c(2,4,6,8,10,12,14,16,18,20,22,24), nrow=2, dimnames=dimnames)
+animal
+
